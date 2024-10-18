dab8ef63bb644e30561b6c63fcb4c43f9d54c45f
Anatolii <anatolii.lopatiuk@itcomtrade.com>
2024-10-18 12:21:01 +0200

    Done With Footer

--- scripts/burger_open_mobile.js
+++ scripts/burger_open_mobile.js
@@ -1,9 +1,12 @@
 function toggleMenu() {
-    var navLinks = document.getElementById('nav-links');
+    var navLinks = document.getElementsByClassName('nav-links')[0]; // Получаем первый элемент
+    var content = document.getElementsByClassName('main-content')[0];
     if (navLinks.classList.contains('open')) {
         navLinks.classList.remove('open');
+        content.classList.remove('menu-open');
     } else {
         navLinks.classList.add('open');
+        content.classList.add('menu-open');
     }
 }
 
